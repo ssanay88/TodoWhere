eb8a11ac491d609a8c9b9a5b4f838347bb819f23
Yangsuhyuck <pplloo748@gmail.com>
2021-08-31 19:08:50 +0900

    21.08.31 커밋 <진행 단계> 리사이클러뷰 완성 <해야 할 것> WorkManager를 통한 백그라운드에서 위치 업데이트 -> LocationWorker.kt에서 위치 권한에 대한 조건 확인 후 거부 시 취할 조치 구현
    세팅에 접근하여 권한 허용 함수 구현 중
    
    reverse geocoding 적용 -> Retrofit을 활용한 API 사용

--- app/src/main/java/com/example/todowhere/MainActivity.kt
+++ app/src/main/java/com/example/todowhere/MainActivity.kt
@@ -4,8 +4,10 @@ import android.Manifest
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -29,7 +31,8 @@ class MainActivity : AppCompatActivity() {
     // 오늘 날짜로 캘린더 객체 생성
     val calendar: Calendar = Calendar.getInstance()
     var TAG: String = "로그"
-    val PERMISSIONS_REQUEST_CODE = 100
+    val FINE_LOTATION_REQUEST_CODE = 100
+    val FINE_BACKGROUND_REQUEST_CODE = 200
 
     // realm 사용을 위한 객체 선언
     val realm = Realm.getDefaultInstance()
@@ -56,7 +59,10 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG,"MainActivity 시작")
 
+        // 권한을 확인하는 함수
+
         // 위치 권환 요청
+        UpdateLocation()
 
 
         selected_date = getDate(selected_year,selected_month,selected_day)
@@ -224,19 +230,21 @@ class MainActivity : AppCompatActivity() {
 
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
             // 권한이 거부했을 경우
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),PERMISSIONS_REQUEST_CODE)
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),FINE_LOTATION_REQUEST_CODE)
 
 
         } else {
             // 권한이 허용된 경우
+            Log.d(TAG,"FINE_LOCATION 접근 권한 허용")
         }
 
         if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_DENIED) {
             // 권환을 거부했을 경우
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),PERMISSIONS_REQUEST_CODE)
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),FINE_LOTATION_REQUEST_CODE)
 
         } else {
             // 권한이 허용된 경우
+            Log.d(TAG,"BACKGROUND_LOCATION 접근 권한 허용")
         }
 
     }
@@ -247,24 +255,48 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         when (requestCode) {
-            PERMISSIONS_REQUEST_CODE -> {
-                if (grantResults.isEmpty()) {
-                    throw RuntimeException("Empty permission result")
-                }
+            FINE_LOTATION_REQUEST_CODE -> {
+//                if (grantResults.isEmpty()) {
+//                    Log.d(TAG,"권한 결과가 비었음")
+//                    // throw RuntimeException("Empty permission result")
+//                }
 
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // 권한이 허용된 경우
+                    Log.d(TAG,"권한 허용")
                 } else {
                     // 권한이 거부된 경우( Deny 버튼 )
                     if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
                         Log.d(TAG,"사용자가 권한 거부")
-                        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),PERMISSIONS_REQUEST_CODE)
+                        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),FINE_LOTATION_REQUEST_CODE)
                     } else {
                         // 권한이 거부되고 다시 묻지 않음 ( Deny & Don't ask again)
                         Log.d(TAG,"사용자가 권한 거부 및 다시 묻지 않기")
                         // 세팅으로 가서 무조건 허용하도록 작성성
                         GetPermission()
                    }
+                }
+            }
+
+            FINE_BACKGROUND_REQUEST_CODE -> {
+//                if (grantResults.isEmpty()) {
+//                    Log.d(TAG,"권한 결과가 비었음1")
+//                    throw RuntimeException("Empty permission result")
+//                }
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한이 허용된 경우
+                } else {
+                    // 권한이 거부된 경우( Deny 버튼 )
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                        Log.d(TAG,"사용자가 권한 거부")
+                        requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),FINE_BACKGROUND_REQUEST_CODE)
+                    } else {
+                        // 권한이 거부되고 다시 묻지 않음 ( Deny & Don't ask again)
+                        Log.d(TAG,"사용자가 권한 거부 및 다시 묻지 않기")
+                        // 세팅으로 가서 무조건 허용하도록 작성성
+                        GetPermission()
+                    }
                 }
             }
         }
@@ -276,9 +308,18 @@ class MainActivity : AppCompatActivity() {
         builder.setTitle("권한 필수 요청")
             .setMessage("위치 서비스를 이용하기 위해 권한이 반드시 허용되어야 합니다.")
 
-        builder.setPositiveButton("OK") {
-
+        builder.setPositiveButton("OK") { dialogInterface, i ->
+            val intent = Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", packageName, null))
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)   // 6
         }
+        builder.setNegativeButton("Later") { dialogInterface, i ->
+            // ignore
+        }
+        val dialog = builder.create()
+        dialog.show()
 
     }
 
